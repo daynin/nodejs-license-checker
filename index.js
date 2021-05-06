695e8641de32058d40db56d62a0b5686c0dc55f2
Evgeny Chirko <zhenya_chirko@mail.ru>
2021-05-06 18:22:13 +0300

    Use license-checker fork

--- index.js
+++ index.js
@@ -1,14 +1,38 @@
 const core = require('@actions/core')
-const github = require('@actions/github')
 const licenseChecker = require('license-checker')
 
-try {
-  const allowedLicenses = core.getInput('allow-only')
+const checkLicenses = (path) => {
+  return new Promise((resolve) => {
+    const allowedLicenses = core.getInput('allow-only')
+    const excludePackages = core.getInput('exclude-packages')
+    try {
+      licenseChecker.init({
+        start: path,
+        onlyAllow: allowedLicenses,
+        excludePackages: excludePackages
+      }, err => {
+        resolve(err);
+      })
+    } catch (error) {
+      resolve(error);
+    }
+  });
+}
 
-  licenseChecker.init({
-    start: './',
-    onlyAllow: allowedLicenses,
-  }, err => err && core.setFailed(err.message))
+try {
+  const paths = core.getInput('paths').split(';');
+
+  Promise.all(paths.map(checkLicenses)).then((pathsErrors) => {
+    if (pathsErrors.filter(Boolean).length) {
+      pathsErrors.forEach((pathCheckErrors, index) => {
+        if (pathErrors) {
+          console.log(`Found errors while check path "${paths[index]}"`);
+          console.log(pathCheckErrors);
+        }
+      })
+      core.setFailed('Licenses check has been failed')
+    }
+  })
 } catch (error) {
   core.setFailed(error.message)
 }
