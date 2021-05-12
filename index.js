8b97518666d9c82bfdd07d9406535088f23b935f
Evgeny Chirko <zhenya_chirko@mail.ru>
2021-05-12 09:46:05 +0300

    Fix reference error

--- index.js
+++ index.js
@@ -27,13 +27,17 @@ try {
   Promise.all(paths.map(checkLicenses)).then((pathsErrors) => {
     if (pathsErrors.filter(Boolean).length) {
       pathsErrors.forEach((pathCheckErrors, index) => {
-        if (pathErrors) {
+        if (pathCheckErrors) {
           console.log(`Found errors while check path "${paths[index]}"`);
           console.log(pathCheckErrors);
         }
       })
       core.setFailed('Licenses check has been failed')
+    } else {
+      console.log('Success')
     }
+  }).catch((error) => {
+    core.setFailed(error.message)
   })
 } catch (error) {
   core.setFailed(error.message)
